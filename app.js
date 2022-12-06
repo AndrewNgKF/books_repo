61c103b65adaabb3ff4bbfaabde89452526dedd0
andrew <andrew@andrew.com>
2022-12-06 20:14:30 +0800

    fixed bug where morgan log wasnt showing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
 import morgan from "morgan";
 import helmet from "helmet";
@@ -6,25 +7,28 @@ import helmet from "helmet";
 import mainRouter from "./routes/mainRoutes.js";
 import bookRouter from "./routes/bookRoutes.js";
 
-const app = express();
-app.use(helmet());
+dotenv.config({ path: ".env" });
 
+const app = express();
+
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
 
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
 });
+
 app.use("/api", limiter);
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use("/healthcheck", (req, res) => {
   res.send("OK");
